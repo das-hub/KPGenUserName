78821005969cf081761cd171356bf2e0c0d23d03
das <das.subscriber@gmail.com>
2019-04-08 15:24:16 +0300

    Add project files.

---
+++ KPGenUserName/CustomConfigEx.cs
@@ -0,0 +1,17 @@
+﻿using KeePass.App.Configuration;
+
+namespace KPGenUserName
+{
+    public static class CustomConfigEx
+    {
+        public static void SetCurrentTransliterationName(this AceCustomConfig customConfig, string name)
+        {
+            customConfig.SetString("CurrentTransliteration", name);
+        }
+
+        public static string GetCurrentTransliterationName(this AceCustomConfig customConfig, string defaultValue = "")
+        {
+            return customConfig.GetString("CurrentTransliteration", defaultValue);
+        }
+    }
+}
---
+++ KPGenUserName/Extensions/StringEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class StringEx
+    {
+        public static string ToShortTitle(this string title)
+        {
+            string[] parts = title.ToLower().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (parts.Length)
+            {
+                case 3:
+                    return $"{parts[1][0]}.{parts[2][0]}.{parts[0]}";
+                case 2:
+                    return $"{parts[1][0]}.{parts[0]}";
+                default:
+                    return title.ToLower();
+            }
+        }
+    }
+}
